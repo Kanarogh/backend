64f6e09006809b1264b6d3ba143700b383bcac14
Jhonatas Teixeira <jhonatas@statum.com.br>
2023-12-13 20:47:44 -0300

    aula2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FluttersModule } from './flutters/flutters.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [FluttersModule],
+  imports: [FluttersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
